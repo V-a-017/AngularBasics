451eac42cdbdffe946145c486a2b21079945f0cf
Name <s@g>
2022-09-29 18:46:12 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-demo';
+  title = 'My demo app';
+  number1=0;
+  number2=0;
+  count = 5;
+
+
+  increment = () => {
+    this.count++;
+  }
+
+
+  decrement= () => {
+    this.count--;
+  }
+
+  
+  listOfNumbers = [1,2,3,4,5];
+  listOfName=["abc","def","ghi"];
+
+
+  sum=0;
+  add = () => {
+  this.sum=this.number1+this.number2;
+  }
+
 }
